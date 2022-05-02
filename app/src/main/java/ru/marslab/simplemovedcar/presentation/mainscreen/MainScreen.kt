edada3387cefb9cab7ac499cf85ac79c60102edc
Sergey Aristov <aristov775@gmail.com>
2022-05-02 19:07:35 +0300

    release home work

--- app/src/main/java/ru/marslab/simplemovedcar/presentation/mainscreen/MainScreen.kt
+++ app/src/main/java/ru/marslab/simplemovedcar/presentation/mainscreen/MainScreen.kt
@@ -1,14 +1,33 @@
 package ru.marslab.simplemovedcar.presentation.mainscreen
 
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.androidx.AndroidScreen
 import cafe.adriel.voyager.hilt.getViewModel
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
+import ru.marslab.simplemovedcar.R
+import ru.marslab.simplemovedcar.presentation.mainscreen.model.MainScreenAction
+
+private const val CAR_HALF_WIDTH = 88
+private const val CAR_HALF_HEIGHT = 44
+private const val ANIMATION_RATIO = 4f
 
 class MainScreen : AndroidScreen() {
 
@@ -22,9 +41,43 @@ class MainScreen : AndroidScreen() {
 
 @Composable
 private fun MainScreenView(viewModel: MainViewModel) {
-    Surface(
-        color = Color.LightGray,
-        modifier = Modifier.fillMaxSize()
+    val configuration = LocalConfiguration.current
+    val screenWidthDp = configuration.screenWidthDp - CAR_HALF_WIDTH
+    val screenHeightDp = configuration.screenHeightDp - CAR_HALF_HEIGHT
+    val state = viewModel.state.collectAsState()
+    val isMoved = state.value.isMoved
+    val animation = spring<Dp>(
+        dampingRatio = ANIMATION_RATIO,
+        stiffness = Spring.StiffnessMedium
+    )
+    val paddingX = animateDpAsState(
+        targetValue = if (isMoved) {
+            Dp(state.value.startPosition.x.toFloat()) * screenWidthDp
+        } else {
+            Dp(state.value.stopPosition.x.toFloat()) * screenWidthDp
+        },
+        animationSpec = animation
+    )
+    val paddingY = animateDpAsState(
+        targetValue = if (isMoved) {
+            Dp(state.value.startPosition.y.toFloat()) * screenHeightDp
+        } else {
+            Dp(state.value.stopPosition.y.toFloat()) * screenHeightDp
+        },
+        animationSpec = animation
+    )
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.LightGray)
     ) {
+        Image(
+            painter = painterResource(id = R.drawable.car),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(start = paddingX.value, top = paddingY.value)
+                .requiredSize(88.dp)
+                .clickable { viewModel.sendAction(MainScreenAction.CarStartClick) }
+        )
     }
 }
--- app/src/main/java/ru/marslab/simplemovedcar/presentation/mainscreen/MainViewModel.kt
+++ app/src/main/java/ru/marslab/simplemovedcar/presentation/mainscreen/MainViewModel.kt
@@ -1,11 +1,13 @@
 package ru.marslab.simplemovedcar.presentation.mainscreen
 
+import android.graphics.Point
 import dagger.hilt.android.lifecycle.HiltViewModel
 import ru.marslab.simplemovedcar.core.BaseViewModel
 import ru.marslab.simplemovedcar.presentation.mainscreen.model.MainScreenAction
 import ru.marslab.simplemovedcar.presentation.mainscreen.model.MainScreenEvent
 import ru.marslab.simplemovedcar.presentation.mainscreen.model.MainScreenState
 import javax.inject.Inject
+import kotlin.random.Random
 
 @HiltViewModel
 class MainViewModel @Inject constructor() :
@@ -13,7 +15,18 @@ class MainViewModel @Inject constructor() :
     override fun reduceStateByAction(
         currentState: MainScreenState,
         action: MainScreenAction
-    ): MainScreenState {
-        return currentState
-    }
+    ): MainScreenState =
+        when (action) {
+            MainScreenAction.CarStartClick -> {
+                val carPosition = state.value.stopPosition
+                state.value.copy(
+                    startPosition = carPosition,
+                    stopPosition = Point(
+                        if (Random.nextBoolean()) 1 else 0,
+                        if (Random.nextBoolean()) 1 else 0
+                    ),
+                    isMoved = true
+                )
+            }
+        }
 }
